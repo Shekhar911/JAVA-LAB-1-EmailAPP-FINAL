5b4ad6c32a98087b58374027f7caf2f532c327f2
Shekhar911 <94576159+Shekhar911@users.noreply.github.com>
2022-03-31 23:39:39 +0530

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,54 @@
+package LabProject;
+import java.util.Scanner;
+public class MainClass {
+
+	public static void main(String[] args) {
+
+		Employee employee = new Employee("Chandra", "Shekhar");
+		CredentialService cs = new CredentialService();
+		String generatedEmail;
+		char[] generatedPassword;
+
+		System.out.println("Please enter the department from the following ");
+		System.out.println("1. Technical");
+		System.out.println("2. Admin");
+		System.out.println("3. Human Resource");
+		System.out.println("4. Legal");
+
+		Scanner sc = new Scanner(System.in);
+		int option = sc.nextInt();
+
+		if (option == 1) {
+			generatedEmail = cs.generateEmailAddress(employee.getFirstName().toLowerCase(),
+					employee.getLastName().toLowerCase(), "Technical");
+			generatedPassword = cs.generatePassword();
+			cs.showCredentials(employee, generatedEmail, generatedPassword);
+		}
+		
+		else if (option == 2) {
+			generatedEmail = cs.generateEmailAddress(employee.getFirstName().toLowerCase(),
+					employee.getLastName().toLowerCase(), "Admin");
+			generatedPassword = cs.generatePassword();
+			cs.showCredentials(employee, generatedEmail, generatedPassword);
+		}
+		
+		else if (option == 3) {
+			generatedEmail = cs.generateEmailAddress(employee.getFirstName().toLowerCase(),
+					employee.getLastName().toLowerCase(), "Human Resource");
+			generatedPassword = cs.generatePassword();
+			cs.showCredentials(employee, generatedEmail, generatedPassword);
+		}
+
+		else if (option == 4) {
+			generatedEmail = cs.generateEmailAddress(employee.getFirstName().toLowerCase(),
+					employee.getLastName().toLowerCase(), "Legal");
+			generatedPassword = cs.generatePassword();
+			cs.showCredentials(employee, generatedEmail, generatedPassword);
+		}
+		
+		else
+			System.out.println("Please opt between 1 to 4 only.");
+		
+		sc.close();
+	}
+}
